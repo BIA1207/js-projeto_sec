07e0cae2c2454b00afbac915aa6ac8be1149ce4a
Bia Carvalho <bianca.cds.bianca@GMAIL.COM>
2025-03-21 21:51:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-let titulo = document.querySelector('h1');
-let titulo2 = document.querySelector('p');
+exibirTextoNaTela('h1', 'Jogo de adivinhação');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
-titulo.innerHTML = 'jogo da adivinhação';
-titulo2.innerHTML = 'Escolha um número de 1 a 100';
-function exibirTextoNaTela(tag,texto){
-    let Campo = document.querySelector(tag)
-    Campo.innerHTML = texto; 
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+
+let tentativas = 3;
+
 function verificarChute() {
-console.log('apertou o botão');
+    let chute = parseInt(document.querySelector('input').value);
+    if (tentativas === 0) {
+        exibirTextoNaTela('h1', 'Você não tem mais tentativas');
+    } else {
+        tentativas--;
+        if (chute === numeroSecreto) {
+            exibirTextoNaTela('h1', 'Parebéns, você acertou');
+        } else {
+            exibirTextoNaTela('h1', 'Você errou');
+        }
+    }
+
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100) + 1
+}
+
+let numeroSecreto = gerarNumeroAleatorio()
+console.log(numeroSecreto);
+
+
+
+
+
+
+
+
+
+
+
+
